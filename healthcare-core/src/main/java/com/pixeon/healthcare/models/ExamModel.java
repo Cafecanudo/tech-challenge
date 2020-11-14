72b22099cb405ca91950f7f0d0bfeacf0327e70b
Wellton S. Barros <cafecanudo@gmail.com>
2020-11-14 11:13:21 -0300

    Refatorando esta verificacao pois nao e responsabilidade do caso de uso verificar os dados do modelo

--- healthcare-core/src/main/java/com/pixeon/healthcare/models/ExamModel.java
+++ healthcare-core/src/main/java/com/pixeon/healthcare/models/ExamModel.java
@@ -76,4 +76,20 @@ public class ExamModel {
     public void setProcedureName(String procedureName) {
         this.procedureName = procedureName;
     }
+
+    public boolean isNullId() {
+        return getId() == null;
+    }
+
+    public boolean isEmptyFields() {
+        return this.getPatientName() == null ||
+                this.getPatientName().isEmpty() ||
+                this.getProcedureName() == null ||
+                this.getProcedureName().isEmpty() ||
+                this.getPhysicianName() == null ||
+                this.getPhysicianName().isEmpty() ||
+                this.getPatientAge() == null ||
+                this.getPatientGender() == null ||
+                this.getPhysicianCRM() == null;
+    }
 }
